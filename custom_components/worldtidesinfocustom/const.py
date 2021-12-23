aabd19d3b9e05309ffadb1ba13bb3a32bac28431
jugla <jugla@free.fr>
2021-12-23 22:59:19 +0100

    add device server name

--- custom_components/worldtidesinfocustom/const.py
+++ custom_components/worldtidesinfocustom/const.py
@@ -98,9 +98,12 @@ ROUND_HOUR = 2
 # Half Tide Slack Duration in seconds
 HALF_TIDE_SLACK_DURATION = 3600
 
-# set constant to give suffix to camera and sensor name
+# set constant to give suffix to camera name
 CAMERA_PLOT_PICTURE_SUFFIX = "_plot_picture"
 CAMERA_CURVE_PICTURE_SUFFIX = "_curve_picture"
+# set constant to give suffix to device name
+DEVICE_SERVER_SUFFIX = "_server"
+# set constant to give suffix to sensor name
 SENSOR_CURRENT_TIDE_HEIGHT_SUFFIX = "_current_tide_height"
 SENSOR_NEXT_LOW_TIDE_HEIGHT_SUFFIX = "_next_low_tide_height"
 SENSOR_NEXT_LOW_TIDE_TIME_SUFFIX = "_next_low_tide_time"
